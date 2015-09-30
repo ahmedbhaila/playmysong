c776a5e72b75be8d3d4dd901762f6e824a91465d
Ahmed Bhaila <ahmedajmal.bhaila@hyatt.com>
2015-09-29 19:10:14 -0500

    Thank you message when sending votes via text

--- src/main/java/com/mycompany/dialtune/NexmoService.java
+++ src/main/java/com/mycompany/dialtune/NexmoService.java
@@ -22,14 +22,14 @@ public class NexmoService {
     @Value("${nexmo.api.secret}")
     private String nexmoApiSecret;
     
-    @Value("${nexmo.phone.number}")
+    @Value("${nexmo.sender.phone.number}")
     private String nexmoNumber;
     
     @Autowired
     RestTemplate restTemplate;
 	
 	private static final String NEXMO_NUMBER_API_URL = "https://api.nexmo.com/number/lookup/json?api_key={api_key}&api_secret={api_secret}&number={number}";
-	private static final String NEXMO_SMS_API_URL = "https://rest.nexmo.com/sms/json?api_key={api_key}&api_secret={api_secret}";
+	private static final String NEXMO_SMS_API_URL = "https://rest.nexmo.com/sms/json?api_key={api_key}&api_secret={api_secret}&from={from}&to={to}&text={text}";
 	private static final String THANK_YOU_MESSAGE = "Thanks for voting";
 	
 	public String getCountryCode(String phoneNubmer) {
@@ -42,17 +42,6 @@ public class NexmoService {
 	}
 	
 	public void sendSMS(String phoneNumber) {
-		HttpHeaders headers = new HttpHeaders();
-		headers.set("Content-Type", "application/x-www-form-urlencoded");
-		
-		JSONObject request = new JSONObject();
-		request.put("from", nexmoNumber);
-		request.put("to", phoneNumber);
-		request.put("text", THANK_YOU_MESSAGE);
-		
-		HttpEntity<String> entity = new HttpEntity<String>(request.toJSONString(), headers);
-
-		restTemplate.exchange(NEXMO_SMS_API_URL, HttpMethod.POST, entity, String.class, nexmoApiKey, nexmoApiSecret);
-		
+		restTemplate.getForEntity(NEXMO_SMS_API_URL, String.class, nexmoApiKey, nexmoApiSecret, nexmoNumber, phoneNumber, THANK_YOU_MESSAGE);	
 	}
 }
--- src/main/java/com/mycompany/dialtune/PollResponseHandler.java
+++ src/main/java/com/mycompany/dialtune/PollResponseHandler.java
@@ -93,6 +93,7 @@ public class PollResponseHandler {
 				handleLocationData(currentCampaign, from);
 			}
 		}
+		nexmoService.sendSMS(from);
 		
 	}
 	
